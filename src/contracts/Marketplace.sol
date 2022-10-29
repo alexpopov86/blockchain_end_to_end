58fe319b0dfb09986ac4eb65b8fd6aab7c49a12a
alexpopov86 <alexanderpopov1986@outlook.com>
2022-10-29 15:57:08 +0300

    Marketplace contract added

---
+++ src/contracts/Marketplace.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity  ^0.5.0;
+
+contract Marketplace {
+    string public name;
+
+    constructor() public {
+        name = "Alex MarketPlace";
+    }
+}
